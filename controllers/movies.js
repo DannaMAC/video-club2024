ec741046a42359dfcaf3fa266d3700e049686a5f
DannaMAC <116323415+DannaMAC@users.noreply.github.com>
2024-10-09 21:10:07 -0600

    Modificación del modelo REST

--- controllers/movies.js
+++ controllers/movies.js
@@ -32,19 +32,49 @@ function addActor(req, res, next){
 }
 
 function index(req, res, next){
-    res.send(`GET => /users/${req.body.id}`)
+    const id=req.params.id;
+    Movie.findByPk(id)
+        .then(object =>res.json(object))
+        .catch(ex => res.send(ex));
 }
 
 function replace(req, res, next){
-    res.send('PUT => /users/:id')
+    const id= req.params.id;
+    Movie.findByPk(id)
+            .then(object =>{
+                const title= req.body.title ? req.body.title : ""; 
+                const genreId= req.body.genreId ? req.body.genreId : ""; 
+                const directorId= req.body.directorId ? req.body.directorId : ""; 
+                object.update({
+                    title: title,
+                    genreId: genreId,
+                    directorId: directorId
+                }).then(objects => res.json(object))
+                  .catch(ex => res.send(ex)); 
+            }).catch(ex => res.send(ex));
 }
 
 function update(req, res, next){
-    res.send('PATCH => /users/:id')
+    const id= req.params.id;
+    Movie.findByPk(id)
+            .then(object =>{
+                const title= req.body.title ? req.body.title : object.title; 
+                const genreId= req.body.genreId ? req.body.genreId : object.genreId; 
+                const directorId= req.body.directorId ? req.body.directorId : object.directorId; 
+                object.update({
+                    title: title,
+                    genreId: genreId,
+                    directorId: directorId
+                }).then(objects => res.json(object))
+                  .catch(ex => res.send(ex)); 
+            }).catch(ex => res.send(ex));
 }
 
 function destroy(req, res, next){
-    res.send('DELETE => /users/:id')
+    const id= req.params.id;
+    Actor.destroy({where: {id : id }})
+    .then(object => res.json(object))
+    .catch(ex => res.send(ex))
 }
 
 module.exports = {create, list, index, replace, update, destroy, addActor};
